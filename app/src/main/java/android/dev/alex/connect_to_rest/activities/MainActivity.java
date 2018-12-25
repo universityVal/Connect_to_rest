1fd759f84e7b5301068aec83979731a7ee3c7088
sasha.lazoriak <sasha.lazoryak@mail.ru>
2018-12-25 21:07:49 +0200

    Navigation to screan University

--- app/src/main/java/android/dev/alex/connect_to_rest/activities/MainActivity.java
+++ app/src/main/java/android/dev/alex/connect_to_rest/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package android.dev.alex.connect_to_rest.activities;
 
+import android.content.Intent;
 import android.dev.alex.connect_to_rest.R;
+import android.dev.alex.connect_to_rest.model.University;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -82,6 +84,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            Intent intent = new Intent(MainActivity.this,University.class);
+            startActivity(intent);
+
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
